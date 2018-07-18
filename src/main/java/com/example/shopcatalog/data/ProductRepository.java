743659a9422ca2e0d434e23eb19c3405e6534ac3
kazhamiakin <kazhamiakin@gmail.com>
2018-07-18 18:27:46 +0200

    Cleanup

--- src/main/java/com/example/shopcatalog/data/ProductRepository.java
+++ src/main/java/com/example/shopcatalog/data/ProductRepository.java
@@ -3,12 +3,10 @@ package com.example.shopcatalog.data;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import com.example.shopcatalog.domain.Product;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
 
-//	@Query("{category: ?1}")
 	Page<Product> findByCategory(String category, Pageable pageRequest);
 }
